82cf362e6442229c9fd976ce8e905877908a98f7
zgoort <zgoort@gmail.com>
2015-06-07 01:24:48 +0300

    Upload

---
+++ cms/webapp/src/main/java/com/run/cms/webapp/engine/Starter.java
@@ -0,0 +1,31 @@
+package com.run.cms.webapp.engine;
+
+import org.jsondoc.spring.boot.starter.EnableJSONDoc;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author Hikmat
+ *
+ */
+//@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+@EnableJSONDoc
+public class Starter extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Starter.class);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Starter.class, args);
+	}
+}
