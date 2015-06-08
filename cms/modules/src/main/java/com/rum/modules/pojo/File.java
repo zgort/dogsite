f8431b7ee095047e20e74634a53224fdfaf77b25
zgoort <zgoort@gmail.com>
2015-06-09 00:51:12 +0300

    Add Setters/Getters

--- cms/modules/src/main/java/com/rum/modules/pojo/File.java
+++ cms/modules/src/main/java/com/rum/modules/pojo/File.java
@@ -1,10 +1,51 @@
 package com.rum.modules.pojo;
 
 public class File {
-	
+
 	private Long id;
 	private String fileName;
 	private FileType fileType;
-	
-	
+	private String desc;
+	private String referance;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public FileType getFileType() {
+		return fileType;
+	}
+
+	public void setFileType(FileType fileType) {
+		this.fileType = fileType;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getReferance() {
+		return referance;
+	}
+
+	public void setReferance(String referance) {
+		this.referance = referance;
+	}
+
 }
